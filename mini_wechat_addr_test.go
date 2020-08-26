16f0b06b97a3fa240c0015622b92ce54d58f4ec9
snowlyg <569616226@qq.com>
2020-08-26 15:09:41 +0800

    完成护工单元测试

--- mini_wechat_addr_test.go
+++ mini_wechat_addr_test.go
@@ -269,3 +269,22 @@ func TestMiniWechatAddrDeleteError(t *testing.T) {
 	obj.Value("code").Equal(400)
 	obj.Value("message").String().Equal("错误数据")
 }
+
+func TestMiniWechatAddrDeleteNoExistError(t *testing.T) {
+	e := httpexpect.WithConfig(httpexpect.Config{
+		Reporter: httpexpect.NewAssertReporter(t),
+		Client: &http.Client{
+			Jar: httpexpect.NewJar(), // used by default if Client is nil
+		},
+		BaseURL: config.Config.Url,
+	})
+	obj := e.DELETE("/api/v1/outline/addr/{id}", 999999).
+		WithHeaders(map[string]string{"X-Token": MiniWechatToken, "IsDev": "1", "AuthType": strconv.FormatInt(int64(common.AUTH_TYPE_MINIWECHAT), 10)}).
+		WithCookie("PHPSESSID", MINIWECHATPHPSESSID).
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+
+	obj.Keys().ContainsOnly("code", "data", "message")
+	obj.Value("code").Equal(400)
+	obj.Value("message").String().Equal("数据不存在")
+}
